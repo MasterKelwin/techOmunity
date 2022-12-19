7e3042fa4281edaa4d3e8d8b195d32c17ca29bfd
Kelwin Hollunder <kelwin.hollunder@fengbrasil.com.br>
2022-12-19 17:10:58 -0300

    intializing login component and home module

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+
+
+export class LoginComponent {
+
+  ngOnInit(): void {
+
+  }
+
+  verifyUser(user: string, password: string) {
+    
+  }
+
+}
+
+
