c011cbce05e654d23ba09d36ce8373337be74e9e
Kelwin Hollunder <kelwin.hollunder@fengbrasil.com.br>
2022-12-30 19:07:36 -0300

    signup component created with his route and post method

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: '', 
     pathMatch:'full', 
-    component:HomeComponent, 
+    component: HomeComponent, 
     children: [{
       path: '',
       component: LoginComponent
-    }]}
+    }]
+  },
+  {
+    path: 'signup',
+    pathMatch: 'full',
+    component: HomeComponent,
+    children: [{
+      path: '',
+      component: SignupComponent
+    }]
+  }
 ];
 
 @NgModule({
