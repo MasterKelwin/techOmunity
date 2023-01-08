ed43c667c2197734fc68c5819cc2b0d444d00cbd
Kelwin Hollunder <kelwin.hollunder@fengbrasil.com.br>
2023-01-08 11:53:29 -0300

    refactoring to reactiveForm, because give me more control over the form and because is good to my studies :)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormErrorModule } from '../components/form-error/form-error.module';
 import { SignupComponent } from './signup/signup.component';
 
@@ -19,7 +19,8 @@ import { SignupComponent } from './signup/signup.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    FormErrorModule    
+    FormErrorModule,
+    ReactiveFormsModule    
   ]
 })
 export class HomeModule { }
